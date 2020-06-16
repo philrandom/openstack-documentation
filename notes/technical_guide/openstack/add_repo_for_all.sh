dd3ef9a0fc630909cb5cc040e7bb3adebe87ef15
philrandom <45124716+philrandom@users.noreply.github.com>
2020-06-16 12:10:52 +0200

    note

--- notes/technical_guide/openstack/add_repo_for_all.sh
+++ notes/technical_guide/openstack/add_repo_for_all.sh
@@ -6,8 +6,9 @@ arr=(comp-1 comp-2 comp-3 sql gateway-1 gateway-2)
 for item in ${arr[*]}
 do
 	echo "setting repo for $item"
-	#ssh root@os-$item.unilim.fr `rm /etc/yum.repos.d/packstack_0.repo; dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm; dnf config-manager --set-enabled PowerTools ; dnf repolist`
-	#ssh root@os-$item.unilim.fr `rm /etc/yum.repos.d/packstack_0.repo  ; yum install dnf-plugins-core ; yum config-manager --set-enabled PowerTools`
-	ssh root@os-$item.unilim.fr `rm /etc/yum.repos.d/packstack_0.repo`
+	# NOTE à n'utilisé que si packstack a mal executé l'ajout de repo
+	ssh root@os-$item.unilim.fr rm -f /etc/yum.repos.d/packstack_0.repo
+	# fin NOTE
+	
 	scp CentOS-PowerTools.repo root@os-$item.unilim.fr:/etc/yum.repos.d/.
 done
