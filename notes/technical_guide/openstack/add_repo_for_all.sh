59a75008be97b69618b1ca5dd20a27de78740d56
philrandom <45124716+philrandom@users.noreply.github.com>
2020-06-16 12:16:43 +0200

    modif ground_control

--- notes/technical_guide/openstack/add_repo_for_all.sh
+++ notes/technical_guide/openstack/add_repo_for_all.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# a ne lancer que sur
+# ground_control
 
 # list des noms machines 
 arr=(comp-1 comp-2 comp-3 sql gateway-1 gateway-2)
@@ -10,5 +12,12 @@ do
 	ssh root@os-$item.unilim.fr rm -f /etc/yum.repos.d/packstack_0.repo
 	# fin NOTE
 	
-	scp CentOS-PowerTools.repo root@os-$item.unilim.fr:/etc/yum.repos.d/.
+	scp /etc/yum.repos.d/CentOS-PowerTools.repo root@os-$item.unilim.fr:/etc/yum.repos.d/.
 done
+
+yum install -y puppet hiera openssh-clients tar nc rubygem-json
+systemctl start puppet
+systemctl status puppet
+yum install -y erlang
+
+# lancer packstack
