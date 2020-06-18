ca6469ac736b009693b12a39c3ff6cc8bdb32cae
philrandom <45124716+philrandom@users.noreply.github.com>
2020-06-18 15:46:37 +0200

    Add files via upload

---
+++ notes/technical_guide/openstack/script/install_memcach.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Mets en cache les jetons et permet de les securisé en les cryptants
+#yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+yum install memcached python3-memcached
+#yum update --disablerepo=epel
+
+cat /etc/sysconfig/memcached > /etc/sysconfig/memcached.backup.origin 
+
+sed "s/OPTIONS=\"-l 127.0.0.1,::1\"/OPTIONS=\"-l 127.0.0.1,::1,os-ground-control.unilim.fr\"/" /etc/sysconfig/memcached > /etc/sysconfig/memcached.second
+
+cat /etc/sysconfig/memcached.second > /etc/sysconfig/memcached
+
+systemctl enable memcached.service
+systemctl start memcached.service
